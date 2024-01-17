3d4aaf979893e39888ab55d4d439fa9cfab9028d
Xryak <sergey.igymn@gmail.com>
2024-01-17 10:01:09 +0300

    Файл

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ FILE_NAME = "test_dataset.json"
 openai.api_key = "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"
 assistant_id = "asst_1QTnFga3XfjYvGkM1vWSImc0"
 
+client = OpenAI(api_key="Api key")
+
+def assitent_creation():
+    file = client.files.create(
+        file=open("datasets/test_dataset.json", "rb"),
+        purpose="assistants"
+    )
+
+    assistant = client.beta.assistants.create(
+        name="Trader",
+        instructions="Здесь инструкции типо ты очень крутой трейдер ",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-3.5-turbo-1106",
+        file_ids=[file.id]
+    )
+    return assistant.id
+
 
 def create_thread(ass_id, prompt):
     thread = openai.beta.threads.create()
